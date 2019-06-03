b1357844cf42efd0104d07ea3353ef6583ad5b42
Karsten Bott <Karsten.Bott@bottnet.de>
2019-06-03 11:57:13 +0200

    Update main.go

--- cmd/in/main.go
+++ cmd/in/main.go
@@ -10,8 +10,8 @@ import (
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/storage"
-	"github.com/pivotal-cf/azure-blobstore-resource/api"
-	"github.com/pivotal-cf/azure-blobstore-resource/azure"
+	"github.com/bottkars/azurestack-blobstore-resource/api"
+	"github.com/bottkars/azurestack-blobstore-resource/azure"
 )
 
 func main() {
